be5b7cc8b872113383a82513a197565a5427e63b
minghsu0107 <minghsu0107@gmail.com>
2020-12-29 04:47:28 +0800

    add rabbitmq prometheus

--- rabbitmq/secret.sh
+++ rabbitmq/secret.sh
@@ -1,1 +1,2 @@
 kubectl create secret generic rabbitmq-config --from-literal=erlang-cookie=rabbitmq-k8s-Dem0
+kubectl create secret generic rabbitmq-admin --from-literal==user=user --from-literal==pass=pass
